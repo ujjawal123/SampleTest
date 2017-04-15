1c083252e9212fda4318307dedd9f848371e14d8
bond <cap.ujjawal@gmail>
2017-04-15 11:15:48 +0530

    add Sample Activity

---
+++ app/src/main/java/com/android/demo/sampletest/Sample.java
@@ -0,0 +1,16 @@
+package com.android.demo.sampletest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ujjaw on 15-04-2017.
+ */
+public class Sample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
