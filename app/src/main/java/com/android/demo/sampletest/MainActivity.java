7da8d4d92304f37b560d37b0956d681baba873a2
ujjawal <cap.ujjawal@gmail.com>
2017-04-15 11:00:25 +0530

    Sample Test

---
+++ app/src/main/java/com/android/demo/sampletest/MainActivity.java
@@ -0,0 +1,33 @@
+package com.android.demo.sampletest;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+Fragment fragment;
+    Button frg_test;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        frg_test=(Button)findViewById(R.id.frg_test);
+        frg_test.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        fragment = new SampleFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        // fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+        frg_test.setVisibility(v.GONE);
+    }
+}
---
+++ app/src/main/java/com/android/demo/sampletest/SampleFragment.java
@@ -0,0 +1,25 @@
+package com.android.demo.sampletest;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ujjaw on 15-04-2017.
+ */
+public class SampleFragment extends Fragment {
+
+    View rootView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        rootView = inflater.inflate(R.layout.fragment_test, container, false);
+        //      mTracker.enableAutoActivityTracking(true);
+
+        return rootView;
+    }
+}
