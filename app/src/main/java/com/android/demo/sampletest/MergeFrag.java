dbcf2242f792d6deac35dd8d4f4f47fc39ac32ae
skadoosh <bhara@Bharateshs-iMac.local>
2017-04-15 18:40:36 +0530

    merge test file added

---
+++ app/src/main/java/com/android/demo/sampletest/MergeFrag.java
@@ -0,0 +1,23 @@
+package com.android.demo.sampletest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by bhara on 15/04/17.
+ */
+
+public class MergeFrag extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
+
