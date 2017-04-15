5a9ece1a2f71760de48c9541ef3576397a81eed7
skadoosh <bhara@Bharateshs-iMac.local>
2017-04-15 11:48:04 +0530

    FirstFragment design init

--- app/src/main/java/com/android/demo/sampletest/FirstFragment.java
+++ app/src/main/java/com/android/demo/sampletest/FirstFragment.java
@@ -1,10 +1,28 @@
 package com.android.demo.sampletest;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by bhara on 15/04/17.
  */
 
 public class FirstFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.first_fragment,null);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
 }
