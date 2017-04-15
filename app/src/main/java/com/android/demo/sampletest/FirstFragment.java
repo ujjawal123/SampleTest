f1426df9cef2052fc8384836a3601d8389f530e3
bond <cap.ujjawal@gmail>
2017-04-15 11:59:31 +0530

    added argument

--- app/src/main/java/com/android/demo/sampletest/FirstFragment.java
+++ app/src/main/java/com/android/demo/sampletest/FirstFragment.java
@@ -15,10 +15,10 @@ import android.view.ViewGroup;
 public class FirstFragment extends Fragment {
 
 
-    public static FirstFragment newInstance() {
+    public static FirstFragment newInstance(String data) {
 
         Bundle args = new Bundle();
-
+        args.putString("title", data);
         FirstFragment fragment = new FirstFragment();
         fragment.setArguments(args);
         return fragment;
@@ -27,7 +27,7 @@ public class FirstFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.first_fragment,null);
+        return inflater.inflate(R.layout.first_fragment, null);
 
     }
 
